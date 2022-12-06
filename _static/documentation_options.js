515cefc32960da3fafac68a79716b948edf2f480
shkevin <shkevin@users.noreply.github.com>
2022-12-06 23:24:29 +0000

    Deploying to gh-pages from @ shkevin/KML@398244b7150e119a43c6edc0386f446f2c395c04 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.dev1+g7b2eb7c',
+    VERSION: '0.1.0.dev1+g398244b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
